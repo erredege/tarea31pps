4a415aa05a4de25295e202410c73bc67dcc66bf2
erredege <rosendodg@gmail.com>
2022-04-07 15:31:21 +0000

    1º añado springboot

---
+++ sumador/src/test/java/soy/Rosendo/sumador/SumadorApplicationTests.java
@@ -0,0 +1,13 @@
+package soy.Rosendo.sumador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
